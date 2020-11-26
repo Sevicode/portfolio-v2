36bf09027f1445bc54cd6e0e2a60740b51d7a720
Sevda Amini-Uhde <sevda.amini@gmail.com>
2020-11-26 20:58:36 +1000

    menu overlay styling

--- src/main.js
+++ src/main.js
@@ -2,11 +2,11 @@ const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
-
-const navItems = document.querySelectorAll('nav-item');
+const navItems = document.querySelectorAll('.nav-item');
 
 // Set Initial State of menu
 let showMenu = false;
+
 menuBtn.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
@@ -26,8 +26,10 @@ function toggleMenu() {
         menuNav.classList.remove('show')
         menuBranding.classList.remove('show')
         navItems.forEach(item => item.classList.remove('show'));
+
+         // set menu back to false
+        showMenu = false;
     }
-    // set menu back to false
-    showMenu = false;
+   
 }
 
